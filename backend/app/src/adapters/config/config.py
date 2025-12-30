9b154f4f696aaa34e4ac2eb4057f6ebd10c38b1d
Joern Kalz <joern.kalz@gmail.com>
2025-12-30 17:05:26 +0100

    Fix backend config

--- backend/app/src/adapters/config/config.py
+++ backend/app/src/adapters/config/config.py
@@ -10,10 +10,10 @@ def load_config():
     """Loads configuration from environment variables, secret manager, or prompts the user."""
     load_dotenv()
 
-    if "GOOGLE_API_KEY" not in os.environ:
+    if "GROQ_API_KEY" not in os.environ:
         if "GROQ_SECRET_NAME" in os.environ:
             client = boto3.client("secretsmanager")
             response = client.get_secret_value(SecretId=os.environ["GROQ_SECRET_NAME"])
             os.environ["GROQ_API_KEY"] = response["SecretString"]
         else:
-            os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your Groq API key: ")
+            os.environ["GROQ_API_KEY"] = getpass.getpass("Enter your Groq API key: ")
